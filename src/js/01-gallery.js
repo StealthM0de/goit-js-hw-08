8c8eb4b16420f671c1e0665412abfa03bb12c383
StealthM0de <jedongfallorina@gmail.com>
2023-11-16 23:07:46 +0800

    update

---
+++ src/js/01-gallery.js
@@ -0,0 +1,36 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+// Described in documentation;
+
+import SimpleLightbox from 'simplelightbox';
+// Additional styles import
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryList = document.querySelector('.gallery');
+
+const createGallery = el => {
+  return el
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+`;
+    })
+    .join('');
+};
+
+const photosMarkup = createGallery(galleryItems);
+galleryList.insertAdjacentHTML('beforeend', photosMarkup);
+
+//--------------------------------------------------------
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+lightbox.on('show.simpleLightbox');
