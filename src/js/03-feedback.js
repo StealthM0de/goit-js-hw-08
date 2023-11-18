22fa339e741fce251a1c2c7bd5f82b9bc716dd69
StealthM0de <jedongfallorina@gmail.com>
2023-11-18 23:40:17 +0800

    update code

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,46 +1,40 @@
 import throttle from 'lodash.throttle';
 
-const form = document.querySelector('.feedback-form');
-const storageKey = 'feedback-form-state';
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('[name="email"]');
 
-// Function to save form state to local storage
-const saveStateToLocalStorage = throttle(() => {
-  const formDate = {
-    email: form.elements.email.value,
-    message: form.elements.message.value,
+const messageInput = document.querySelector('[name="message"]');
+
+const saveFormState = throttle(() => {
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
   };
 
-  localStorage.setItem(storageKey, JSON.stringify(FormData));
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }, 500);
 
-// Event listener for input events
-form.addEventListener('input', saveStateToLocalStorage);
+emailInput.addEventListener('input', saveFormState);
+messageInput.addEventListener('input', saveFormState);
 
-// Function to load form state from local storage
-const loadStateFromLocalStorage = () => {
-  const storedDate = localStorage.getItem(storageKey);
-
-  if (storedData) {
-    const formData = JSON.parse(storedData);
-    form.elements.email.value = formData.email || '';
-    form.elements.message.value = formData.message || '';
-  }
-};
-
-// Load form state on page load
-loadStateFromLocalStorage();
-
-// Event listener for form submission
-form.addEventListener('submit', event => {
+feedbackForm.addEventListener('submit', event => {
   event.preventDefault();
 
-  // Display form data in the console
   const formData = {
-    email: form.elements.email.value,
-    message: form.elements.message.value,
+    email: emailInput.value,
+    message: messageInput.value,
   };
+
   console.log(formData);
-  // Clear form fields and local storage
-  form.reset();
-  localStorage.removeItem(storageKey);
+
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
 });
+
+const storedFormState = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+if (storedFormState) {
+  emailInput.value = storedFormState.email;
+  messageInput.value = storedFormState.message;
+}
